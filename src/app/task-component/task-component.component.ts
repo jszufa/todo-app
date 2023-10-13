60b8f5d4b11d8c579c9e6e0eb0064caaf65904d2
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-10-13 13:28:06 +0200

    checkbox two way binding

--- src/app/task-component/task-component.component.ts
+++ src/app/task-component/task-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, } from '@angular/core';
 import { TaskItem } from '../task-item';
 
 @Component({
@@ -9,6 +9,8 @@ import { TaskItem } from '../task-item';
 export class TaskComponentComponent {
   @Input() task!: TaskItem;
   @Output() newDeleteEvent = new EventEmitter<number>();
+
+  isChecked: boolean = false;
 
   deleteTask(id: number) {
     this.newDeleteEvent.emit(id);
